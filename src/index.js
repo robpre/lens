4f1399f0870c1b149679e6056f3acecd2a49b3c2
Robert Preus-MacLaren <rob@neontribe.co.uk>
2018-09-10 19:25:07 +0100

    error handling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import 'webrtc-adapter';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+window.onerror = (err) => {
+  document.getElementById('root').innerHTML = `<pre>${err.message}\n${err.stack}</pre>`;
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
